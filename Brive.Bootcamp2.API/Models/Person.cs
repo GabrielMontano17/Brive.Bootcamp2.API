cd703a182a32820160d34d4c77d2970fbc8a6d0a
Gabo <sonic1794@gmail.com>
2022-05-18 09:20:41 -0500

    Agregar archivos de proyecto.

---
+++ Brive.Bootcamp2.API/Models/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Brive.Bootcamp2.API.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+        public DateTime CreatedDate { get; set; }
+
+    }
+}
